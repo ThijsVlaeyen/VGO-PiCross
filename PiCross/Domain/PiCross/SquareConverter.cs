b2f83f0e6f6d0c04dd5086e3461355deeb21c52b
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2020-03-28 12:24:05 +0100

    Setup

---
+++ PiCross/Domain/PiCross/SquareConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PiCross.PiCross
+{
+    class SquareConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+       {
+           var square = (Square)value;
+           if (square == Square.EMPTY )
+           {
+               return Brushes.White;
+           }
+           else if (square == Square.FILLED )
+           {
+               return Brushes.Black;
+           }
+           else
+           {
+               return Brushes.Gray;
+           }
+       }
+
+       public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+       {
+           throw new NotImplementedException();
+       }
+    }
+}
--- PiCross/View/MainWindow.xaml.cs
+++ PiCross/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructures;
+using PiCross;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Grid = DataStructures.Grid;
+using Size = DataStructures.Size;
+
 namespace View
 {
     /// <summary>
@@ -23,6 +28,22 @@ namespace View
         public MainWindow()
         {
             InitializeComponent();
+
+            var puzzle = Puzzle.FromRowStrings(
+             "xxxxx",
+             "x...x",
+             "x...x",
+             "x...x",
+             "xxxxx"
+            );
+            var facade = new PiCrossFacade();
+            var playablePuzzle = facade.CreatePlayablePuzzle(puzzle);
+
+            playablePuzzle.Grid[new Vector2D(0, 0)].Contents.Value = Square.FILLED;
+            playablePuzzle.Grid[new Vector2D(1, 0)].Contents.Value = Square.EMPTY;
+
+            picrossControl.Grid = playablePuzzle.Grid;
+            picrossControl.RowConstraints = playablePuzzle.RowConstraints;
         }
     }
 }
